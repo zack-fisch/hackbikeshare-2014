533bbf78f623ff0a438afe01f8935465de78e621
Zack Fisch <zack@zfisch.com>
2014-08-16 12:42:21 -0400

    Adding user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  skip_before_filter :require_login, only: [:index, :new, :create]
   def index
     @users = User.all
   end
@@ -11,10 +11,14 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		redirect_to root_path, :notice => 'Signed Up'
+  		redirect_to(:users, notice: 'User was successfully created')
   	else
   		render 'new'
   	end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
