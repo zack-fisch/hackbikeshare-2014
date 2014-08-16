d1a3fa4833833b91fa09e7cf37916f7f428578ae
Josh Cornelius <joshcornelius9@gmail.com>
2014-08-16 18:43:46 -0400

    mapbox update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,17 +1,16 @@
 class WelcomeController < ApplicationController
   def home
-  	 @trips = Trip.where(start_station_id: 7000).limit(200) 
-     @trips += Trip.where(start_station_id: 7005).limit(200) 
-     binding.pry
-  	 @locations = []
+	  @trips = Trip.where(start_station_id: 7000).limit(100)
+    @trips += Trip.where(start_station_id: 7005).limit(100)
 
-  	 @trips.each do |trip|
-  	 	@start_station = Station.find_by(station_id: trip.start_station_id)
-  		@end_station = Station.find_by(station_id: trip.end_station_id)
-  		if @start_station && @end_station
-  	 		@locations << [[@start_station.latitude, @start_station.longitude], [@end_station.latitude, @end_station.longitude]]
-  	 	end
-  	 	@locations
-  	 end
+	  @locations = []
+
+	  @trips.each do |trip|
+	 	  @start_station = Station.find_by(station_id: trip.start_station_id)
+		  @end_station = Station.find_by(station_id: trip.end_station_id)
+		  if @start_station && @end_station
+	 		  @locations << [[@start_station.latitude, @start_station.longitude], [@end_station.latitude, @end_station.longitude]]
+	 	  end
+	  end
   end
 end
