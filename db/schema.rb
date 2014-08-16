d561211ee2d996d1baff1dc1e8b1a9e98b2b211a
Josh Cornelius <joshcornelius9@gmail.com>
2014-08-16 16:47:24 -0400

    merging

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140816192020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "stations", force: true do |t|
     t.string   "station_name"
@@ -35,12 +33,16 @@ ActiveRecord::Schema.define(version: 20140816192020) do
     t.string   "bike_number"
     t.datetime "created_at"
     t.datetime "updated_at"
-
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",            null: false
     t.string   "crypted_password", null: false
     t.string   "salt",             null: false
-
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+
 end
