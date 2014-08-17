e9aa4ad17a6a6e344e0e1f269a53c2fb868825e6
Josh Cornelius <joshcornelius9@gmail.com>
2014-08-16 23:32:59 -0400

    added disdtance between

--- app/controllers/riddles_controller.rb
+++ app/controllers/riddles_controller.rb
@@ -9,9 +9,10 @@ class RiddlesController < ApplicationController
 	end
 
 	def check_in
-		binding.pry
 		@location = params['location']
+		@coords = [36.94159, -82.473447]
 		@riddle = Riddle.find(params[:id])
+		@distance = Geocoder::Calculations.distance_between(@coords, @location, units: :km)
 		redirect_to riddle_path(@riddle)
 	end
 end
