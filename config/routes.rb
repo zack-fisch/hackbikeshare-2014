5c316b8b66787ef37c68b70b6e518aeec125d0b6
davidhurley <dhurls99@gmail.com>
2014-08-17 10:38:30 -0400

    add rules to project

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   get 'login' => 'user_sessions#new'
   get 'logout' => 'user_sessions#destroy'
   get 'map' => 'welcome#map'
+  get 'rules' => 'welcome#rules'
+
 
 
 
